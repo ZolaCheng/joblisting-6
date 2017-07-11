2efc21fea82efea164fa87bf1c0eef2cafa67fca
Zola Chen <zolacheng@gmail.com>
2017-07-11 15:43:46 +0800

    implement part4

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,10 +1,10 @@
 class Job < ApplicationRecord
 
-  validates :title, presence: {message: "请填写标题"}
 
+  validates :title , presence: {message: "请填写标题"}
   validates :wage_upper_bound, presence: {message:"请填写最高薪水"}
   validates :wage_lower_bound, presence: {message:"请填写最低薪水"}
-  validates :wage_lower_bound, numericality: { greater_than: 0， message:"最低薪水必须大于0"}
+  validates :wage_lower_bound, numericality: { greater_than: 0, message: "最低薪水必须大于0"}
 
   validates :wage_lower_bound, numericality: {less_than: :wage_upper_bound, message: "薪水下限不能高于薪水上限"}
 
